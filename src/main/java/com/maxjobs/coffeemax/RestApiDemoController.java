9924d5494138d604376c79986e289330215215e3
Maxwell Adomako <maxwell.adomako@amalitech.com>
2024-03-22 13:25:12 +0000

    mapping configs at class level

--- src/main/java/com/maxjobs/coffeemax/RestApiDemoController.java
+++ src/main/java/com/maxjobs/coffeemax/RestApiDemoController.java
@@ -1,5 +1,7 @@
 package com.maxjobs.coffeemax;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -7,10 +9,11 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * To
+ * Refactoring to reduce repetition in code with method with
  * */
 
 @RestController
+@RequestMapping("/coffees")
 class RestApiDemoController {
     private List<Coffee> coffees = new ArrayList<>();
 
@@ -24,7 +27,9 @@ class RestApiDemoController {
     }
 
     // Get all coffees controller config
-    @GetMapping("/coffees")
+    // takes no argument when @ReequestMapping is specified at the
+    // at the controller level.
+    @GetMapping()  // becomes the base url or home of GET request
     Iterable<Coffee> getCoffees(){
         return coffees;
     }
@@ -35,7 +40,9 @@ class RestApiDemoController {
      * item by ID. it was used to pass the id as a parameter to the
      * getCoffeeById method
      * */
-    @GetMapping("/coffees/{id}")
+    // takes only the '/{id}' parameter when the @RequestMapping is set
+    // at the Controller class level.
+    @GetMapping("/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id){
         for (Coffee c : coffees){
             if(c.getId().equals(id)){
@@ -54,7 +61,9 @@ class RestApiDemoController {
      * it also used the coffees object from Coffee class and augmented it with
      * the coffee data of the postCoffee method
      */
-    @PostMapping("/coffees")
+    // does not take any param because of @RequestMapping
+    // at the Controller class level
+    @PostMapping()
     Coffee postCoffee(@RequestBody Coffee coffee){
         coffees.add(coffee);
         return coffee;
@@ -67,9 +76,12 @@ class RestApiDemoController {
      * Notice the PutMapping requires a PathVariable and a RequestBody
      * The PathVariable is used to get the id of the object if it exists and
      * updates the object, if not, it creates it anew.
+     * Identify how that was achieved using the ResponseEntity<>class
      **/
-    @PutMapping("coffees/{id}")
-    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee){
+    // Takes only the '/{id}' param when the @RequestMapping is set at the
+    // Controller class level.
+    @PutMapping("/{id}")
+    ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee){
         int coffeeIndex = -1;
 
         for(Coffee c: coffees){
@@ -78,7 +90,15 @@ class RestApiDemoController {
                 coffees.set(coffeeIndex, coffee);
             }
         }
-        return (coffeeIndex == -1) ? postCoffee(coffee) : coffee;
+        /**
+         * For our put method, it con do two things of which the results are
+         * all different. Therefor we need to specify the response to know the
+         * type of action that was performed. This is done by using the
+         * ResponseEntity class to identify the action that was performed.
+         * */
+        return (coffeeIndex == -1) ?
+                new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
+                new ResponseEntity<>(coffee, HttpStatus.OK);
     }
     /**
      * DeleteMapping seems different as its a void method
@@ -88,7 +108,9 @@ class RestApiDemoController {
      * removeIf accepts a Predicate, meaning we can provide a lambda that
      * will evaluate a boolean value of true for the desired coffee to remove
      * */
-    @DeleteMapping("/coffees/{id}")
+    // Takes only the '/{id}' param when the @RequestMapping is set at the
+    // Controller class level.
+    @DeleteMapping("/{id}")
     void deleteCoffee(@PathVariable String id){
         coffees.removeIf(c -> c.getId().equals(id));
     }
